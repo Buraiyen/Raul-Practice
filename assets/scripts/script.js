a7c36ab9c3730b51709b3750c88c5e6086bc23a0
IamRaul <jarquinr121@gmail.com>
2023-05-26 22:52:44 -0700

    hamburger fix

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector(".menuIcon");
 
 function openMenu (element) {
     element.classList.toggle('change');
-    let dropDownMenu = document.querySelector('.dropdownMenu');
+    const dropDownMenu = document.querySelector('.dropdownMenu');
     dropDownMenu.classList.toggle('open');
     const isOpen = dropDownMenu.classList.contains('open');
 }
